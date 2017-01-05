fb33147a221d838cde41aab2ba83126e96799be3
Dylan Whichard <dylan@whichard.com>
2017-01-05 17:25:21 -0500

    go

--- server.py
+++ server.py
@@ -198,7 +198,7 @@ class Component(ApplicationSession):
                     except:
                         traceback.print_exc()
                 if time.time() > next_rssi:
-                    send_packet(badge_id, bytes(0, 0, 0, 0, 0, 0, 0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0])
+                    send_packet(badge_id, bytes([0, 0, 0,0,0,0,0,0,0,0,0,0,0,0,0]))
                                 #b"\x00\x00\x00" + struct.pack("bbbbbbbbbbbb", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
                     next_rssi = time.time() + WIFI_INTERVAL
                     #self.rssi_all(30, 45, 96)
