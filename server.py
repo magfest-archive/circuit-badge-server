8b9c3b430b74cb93d2517a9e03bf1459ce81b9c6
Dylan Whichard <dylan@whichard.com>
2017-01-06 03:45:58 -0500

    rssi as test?

--- server.py
+++ server.py
@@ -123,7 +123,8 @@ class Component(ApplicationSession):
     @asyncio.coroutine
     def set_lights_one(self, badge_id, r, g, b):
         print("Setting lights!")
-        executor.submit(self.send_packet, badge_id, bytes((LED_CONTROL, ) + (g, r, b) * 4))
+        self.rssi(badge_id)
+        #executor.submit(self.send_packet, badge_id, bytes((LED_CONTROL, ) + (g, r, b) * 4))
 
     @asyncio.coroutine
     def set_lights(self, badge_id, *colors):
