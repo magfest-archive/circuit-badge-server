a1fe566e0e88935e04b74581a124e09533033364
Dylan Whichard <dylan@whichard.com>
2017-01-06 13:02:20 -0500

    randomize reset updates

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ import pickle
 import concurrent.futures
 import traceback
 import collections
+import random
 
 BUTTON_RIGHT = 1
 BUTTON_DOWN = 2
@@ -185,7 +186,7 @@ class Component(ApplicationSession):
                 if msg_type == STATUS_UPDATE:
                     gpio_state, gpio_trigger, gpio_direction = packet[8], packet[9], packet[10]
 
-                    if not gpio_trigger and not gpio_state and tuple(self.buttons[badge_id]) != KONAMI:
+                    if not gpio_trigger and not gpio_state and random.random() > .66 and tuple(self.buttons[badge_id]) != KONAMI:
                         print("resetting")
                         yield from self.set_lights(badge_id, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
