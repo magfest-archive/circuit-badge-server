1bb143e64072e9f92709ea8b4bb5dae51397fb36
Dylan Whichard <dylan@whichard.com>
2017-01-06 03:43:27 -0500

    temp simpler code

--- gamemgr.py
+++ gamemgr.py
@@ -16,7 +16,7 @@ JOIN_LENGTH = 4
 JOIN_KEYS = ['up', 'down', 'left', 'right', 'a', 'b']
 TOTAL_JOIN = JOIN_LENGTH + len(JOIN_PREFIX)
 
-KONAMI = ('up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a', 'start')
+KONAMI = ('select', 'select', 'start', 'start')#'('up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a', 'start')
 
 JOIN_INDEX_MAX = 6**4 # 1296
 
